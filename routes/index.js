16dd67e6134e90126b6c490c156763a2e1b1ba86
huangj <huangj@yuzhi.cn>
2024-03-26 20:25:22 +0800

    dev

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,10 @@ const router = express.Router();
 const mysql = require('mysql');
 // const domain = "http://t66y.com/";
 // const domain = "https://cl.2718y.xyz/";
-const domain = "https://cl.7362x.xyz/";
-https: //cl.7362x.xyz/
-    const {
-        pager
-    } = require('../utils/pager')
+const domain = "https://cl.2167y.xyz/";
+const {
+    pager
+} = require('../utils/pager')
 
 const confs = {
     local: {
